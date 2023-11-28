911079033d9582140218205994fe8dfc7e4d601a
PrasannaNaik211 <prasannanaik304@gmail.com>
2023-11-28 19:53:31 +0530

    7thCommitAddedVoiceEffectForwrongPlace

--- src/script.js
+++ src/script.js
@@ -21,6 +21,11 @@ async function checkWeather(city) {
   if (response.status == 404) {
     document.querySelector(".error").style.display = "block";
     document.querySelector(".weather").style.display = "none";
+
+    // Play the sound for incorrect city
+    audio.src = "/wrongCity.mp3"; 
+     audio.play();
+    console.log("Playing sound for incorrect city");
   } else {
     var data = await response.json();
 
